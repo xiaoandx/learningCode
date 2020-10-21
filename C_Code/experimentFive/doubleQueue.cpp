24b9930ff2dc6843f31be482e32e83737ca4f276
WEI.ZHOU <xiaoandx@gmail.com>
2020-10-21 11:38:23 +0800

    add doubleQueue

--- C_Code/experimentFive/doubleQueue.cpp
+++ C_Code/experimentFive/doubleQueue.cpp
@@ -11,7 +11,7 @@
  * @Author: WEI.ZHOU
  * @Date: 2020-10-20 19:59:47
  * @Version: V1.0
- * @LastEditTime: 2020-10-20 20:21:28
+ * @LastEditTime: 2020-10-21 11:36:52
  * @LastEditors: WEI.ZHOU
  * @Others: 
  */
@@ -35,26 +35,76 @@ typedef struct{
 }QueueLike;
 
 /**
- * @brief 初始化双端队列
- * @date  2020-10-20 20:04:22
- * @param  QueueLike 需要进行初始化的双端链队列
- * @return {int} 返回操作状态，1->成功；0->失败
+ * @brief   初始化双端队列
+ * @date    2020-10-20 20:04:22
+ * @param   QueueLike 需要进行初始化的双端链队列
+ * @return  {int} 返回操作状态，1->成功；0->失败
  */
 int initDoubleQueue(QueueLike &q);
 
 /**
- * @brief 入队
- * @Date: 2020-10-20 20:19:30
- * @param  QueueLike 需要进行操作的双端链队列
- * @param  int data 需要入队的数据
- * @return {int} 返回操作状态，1->成功；0->失败
+ * @brief   入队
+ * @Date    2020-10-20 20:19:30
+ * @param   QueueLike 需要进行操作的双端链队列
+ * @param   int data 需要入队的数据
+ * @return  {int} 返回操作状态，1->成功；0->失败
  */
-int appendDoubleQueue(QueueLike &q,int data);
+int pushDoubleQueue(QueueLike &q,int data);
+
+/**
+ * @brief   出队列
+ * @Date:   2020-10-21 10:48:36
+ * @param   QueueLike 需要进行操作的双端链队列
+ * @param   int value 出队的数据 
+ * @return  {int} 返回操作状态，1->成功；0->失败
+ */
+int popDoubleQueue(QueueLike &q,int &value);
+
+/**
+ * @brief   遍历双端队列
+ * @Date:   2020-10-21 11:14:16
+ * @param   QueueLike 需要进行操作的双端链队列
+ */
+void traverseDoubleQueue(QueueLike q);
+
+/**
+ * @brief   队尾添加元素
+ * @Date:   2020-10-21 11:23:24
+ * @param   QueueLike 需要进行操作的双端链队列
+ * @param   int data 需要入队的数据
+ * @return  {int} 返回操作状态，1->成功；0->失败
+ */
+int rearPushDoubleQueue(QueueLike &q,int data);
+
+/**
+ * @brief   队尾删除元素
+ * @Date:   2020-10-21 11:27:47
+ * @param   QueueLike 需要进行操作的双端链队列
+ * @param   int value 出队的数据 
+ * @return  {int} 返回操作状态，1->成功；0->失败
+ */
+int rearPopDoubleQueue(QueueLike &q,int &value);
+
+/**
+ * @brief   判断双端队列是否为空
+ * @Date:   2020-10-21 11:34:47
+ * @param   QueueLike 需要进行操作的双端链队列 
+ * @return  {int} 返回操作状态，1->不为空；0->为空
+ */
+int emptyDoubleQueue(QueueLike q);
 
 int main(){
     QueueLike q;
+    int value;
     cout<<initDoubleQueue(q)<<endl;
-    appendDoubleQueue(q,12);
+    pushDoubleQueue(q,12);
+    pushDoubleQueue(q,13);
+    traverseDoubleQueue(q);
+    //popDoubleQueue(q,value);
+    //cout<<value<<endl;
+    rearPushDoubleQueue(q,19);
+    traverseDoubleQueue(q);
+    cout<<emptyDoubleQueue(q)<<endl;
 }
 
 int initDoubleQueue(QueueLike &q){
@@ -66,7 +116,85 @@ int initDoubleQueue(QueueLike &q){
     return OPERATION_SUCCESS;
 }
 
-int appendDoubleQueue(QueueLike &q,int data){
+int pushDoubleQueue(QueueLike &q,int data){
+    QuNodePrt middleVariable;
     if(!q.front){ return OPERATION_ERROR;}
-    
+    middleVariable = (QuNodePrt)malloc(sizeof(quNode));
+    if(!middleVariable){ return OPERATION_ERROR;}  
+    middleVariable -> data = data;
+    if(q.front == q.later){
+        q.front -> next = middleVariable;
+        middleVariable -> last = q.front;
+        middleVariable -> next = NULL;
+        q.later = middleVariable;
+        return OPERATION_SUCCESS;
+    }
+    middleVariable -> next = q.front -> next;
+    middleVariable -> last = q.front;
+    q.front -> next -> last = middleVariable;
+    q.front -> next = middleVariable;
+    return OPERATION_SUCCESS; 
+}
+
+int popDoubleQueue(QueueLike &q,int &value){
+    QuNodePrt middleVariable;
+    if(q.front == q.later){ return OPERATION_ERROR;}
+    middleVariable = q.front -> next;
+    value = middleVariable -> data;
+    if(q.front -> next == q.later){
+        q.later = q.front;
+        q.later -> next = NULL;
+        free(middleVariable);
+        return OPERATION_SUCCESS;
+    }
+    middleVariable -> next -> last = q.front;
+    q.front -> next = middleVariable -> next;
+    free(middleVariable);
+    return OPERATION_SUCCESS;
+}
+
+void traverseDoubleQueue(QueueLike q){
+     QuNodePrt middleVariable;
+     middleVariable = q.front -> next;
+     while (middleVariable){
+         cout<<middleVariable -> data<<" ";
+         middleVariable = middleVariable -> next;
+     }
+     cout<<endl;
+}
+
+int rearPushDoubleQueue(QueueLike &q,int data){
+    QuNodePrt middleVariable;
+    if(!q.front){ return OPERATION_ERROR;}
+    middleVariable = (QuNodePrt)malloc(sizeof(quNode));
+    if(!middleVariable){ return OPERATION_ERROR;}  
+    middleVariable -> data = data;
+    if(q.front == q.later){
+        q.front -> next = middleVariable;
+        middleVariable -> last = q.front;
+        middleVariable -> next = NULL;
+        q.later = middleVariable;
+        return OPERATION_SUCCESS;
+    }
+    q.later -> next = middleVariable;
+    middleVariable -> next = NULL;
+    middleVariable -> last = q.later;
+    q.later = middleVariable;
+    return OPERATION_SUCCESS; 
+}
+
+int rearPopDoubleQueue(QueueLike &q,int &value){
+     QuNodePrt middleVariable;
+    if(q.front == q.later){ return OPERATION_ERROR;}
+    middleVariable = q.later;
+    value = middleVariable -> data;
+    q.later = middleVariable -> last;
+    q.later -> next = NULL;
+    free(middleVariable);
+    return OPERATION_SUCCESS;
+}
+
+int emptyDoubleQueue(QueueLike q){
+    if(q.front == q.later){ return OPERATION_ERROR;}
+    return OPERATION_SUCCESS;
 }
