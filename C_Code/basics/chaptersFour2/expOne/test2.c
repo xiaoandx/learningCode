530d07794f82c306b1e745943359c52808a27554
WEI.ZHOU <xiaoandx@gmail.com>
2020-11-10 15:50:17 +0800

    update basercode

--- C_Code/basics/chaptersFour2/expOne/test2.c
+++ C_Code/basics/chaptersFour2/expOne/test2.c
@@ -7,7 +7,7 @@
  * In case of code problems, feedback can be made through the following email
  * address. <xiaoandx@gmail.com>
  *
- * @Description:
+ * @Description: 九九乘法表
  * @Author: WEI.ZHOU
  * @Date: 2020-11-07 16:02:37
  * @Version: V1.0
@@ -17,10 +17,14 @@
  */
 #include <stdio.h>
 int main() {
+    // 第一行字段标题
     printf("  *  1  2  3  4  5  6  7  8  9\n");
+    // 循环
     for(int k = 1;k<=9;k++){
+        // 循环输出每行的第一个数字
         for (int i = k; i<=k; i++) {
             printf("%3d", i);
+            // 循环输出行与每列的乘积
             for (int j = 1; j <=k; j++) {
                 printf("%3d", i * j);
             }
