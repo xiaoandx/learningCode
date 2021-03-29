23224ad9345816c117611ac1a20fae79f1b52d3a
WEI.ZHOU <2324671838@qq.com>
2021-03-29 21:50:33 +0800

    style javaBasicsCode : 修改UDP注释

--- Java_code/javaBasicsCode/src/club/xiaoandx/section9/UDP_Receiver.java
+++ Java_code/javaBasicsCode/src/club/xiaoandx/section9/UDP_Receiver.java
@@ -31,13 +31,16 @@ public class UDP_Receiver {
          // 定义一个DatagramPacket对象，用于接收数据
 		DatagramPacket dp = new DatagramPacket(buf, buf.length);
 		System.out.println("等待接收数据");
-		ds.receive(dp); // 等待接收数据，如果没有数据则会阻塞
+		// 等待接收数据，如果没有数据则会阻塞
+		ds.receive(dp); 
          // 调用DatagramPacket的方法获得接收到的信息
 		
          //包括数据的内容、长度、发送的IP地址和端口号
 		String str = new String(dp.getData(), 0, dp.getLength()) +
          "from "+ dp.getAddress().getHostAddress() + ":" + dp.getPort();
-		System.out.println(str); // 打印接收到的信息
-		ds.close();// 释放资源
+		// 打印接收到的信息
+		System.out.println(str);
+		// 释放资源
+		ds.close();
 	}
 }
