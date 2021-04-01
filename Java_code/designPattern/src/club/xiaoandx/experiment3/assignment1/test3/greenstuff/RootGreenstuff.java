e2cf64a791090fe95534e31cd5178f75e813b91d
WEI.ZHOU <2324671838@qq.com>
2021-04-01 20:13:37 +0800

    feat designPattern : 添加抽象工厂模式实例 - 蔬菜类别接口

---
+++ Java_code/designPattern/src/club/xiaoandx/experiment3/assignment1/test3/greenstuff/RootGreenstuff.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2021 WEI.ZHOU. All rights reserved.
+ * The following code is only used for learning and communication, not for illegal and 
+ * commercial use.
+ * If the code is used, no consent is required, but the author has nothing to do with any problems
+ * and consequences.
+ * In case of code problems, feedback can be made through the following email address.
+ * 
+ *                        <xiaoandx@gmail.com>
+ */
+package club.xiaoandx.experiment3.assignment1.test3.greenstuff;
+
+/**  
+ * <p> 根菜蔬菜  </p> 
+ * @ClassName:RootGreenstuff   
+ * @author: WEI.ZHOU
+ * @date: 2021-04-01 19:34
+ * @since: JDK1.8
+ * @version V1.0.0
+ */
+public interface RootGreenstuff extends Greenstuff {
+
+}
