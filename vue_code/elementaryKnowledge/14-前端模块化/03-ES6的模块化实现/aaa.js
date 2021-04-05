802796bdaccd87cb50a2d99f8aff2490b3c5da73
WEI.ZHOU <2324671838@qq.com>
2021-04-05 19:54:06 +0800

    feat vue : 添加vue学习资料

---
+++ vue_code/elementaryKnowledge/14-前端模块化/03-ES6的模块化实现/aaa.js
@@ -0,0 +1,28 @@
+//1.直接默认导出
+export let name = '小明'
+var age = 22
+
+function sum(num1, num2) {
+  return num1 + num2
+}
+var flag = true
+if (flag) {
+  console.log(sum(10, 20))
+}
+//2.最后统一导出
+export {
+  flag,sum,age
+}
+
+//3.导出函数/类
+export function say(value) {
+  console.log(value);
+}
+
+export class Person{
+  run(){
+    console.log("奔跑");
+  }
+}
+// 4. export default 只能有一个default
+export default let name = '小明'
