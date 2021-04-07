5c39e51ec91de3fb2e90b4c8ec6be7894c41fdfb
WEI.ZHOU <2324671838@qq.com>
2021-04-07 13:23:26 +0800

    feat vue : 添加路由全局守卫钩子 与后置钩子

--- vue_code/elementaryKnowledge/17-vue-router/lear-vue-demo1/src/router/index.js
+++ vue_code/elementaryKnowledge/17-vue-router/lear-vue-demo1/src/router/index.js
@@ -1,3 +1,19 @@
+/*
+ * @explain: Copyright (c) 2020 WEI.ZHOU. All rights reserved.
+ * The following code is only used for learning and communication, not for illegal and commercial use.
+ * If the code is used, no consent is required, but the author has nothing to do with any problems 
+ * and consequences.
+ * 
+ * In case of code problems, feedback can be made through the following email address.
+ * 						<xiaoandx@gmail.com>
+ * 
+ * @Description: 
+ * @Author: WEI.ZHOU
+ * @Date: 2021-04-06 10:19:49
+ * @Version: V1.0
+ * @Others: Running test instructions
+ * 			
+ */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../components/Home.vue'
@@ -23,6 +39,9 @@ const routes = [
   {
     path:'/home',
     component:Home,
+    meta:{
+      title: "首页"
+    },
     children:[
       {
         path: '',
@@ -40,11 +59,17 @@ const routes = [
   },
   {
     path:'/about',
-    component: About
+    component: About,
+    meta:{
+      title: "关于我们"
+    },
   },
   {
     path:'/user/:userId',
-    component:User
+    component:User,
+    meta:{
+      title: "用户"
+    },
   },
   {
     path: '/profile',
@@ -60,5 +85,24 @@ const router = new VueRouter({
   mode: 'history',
   linkActiveClass: 'action'
 })
+
+/**
+ *
+ * 前置守卫：从from跳转到to
+ * from 来的路由
+ * to 要去的路由
+ */
+ router.beforeEach((to, from, next) => {
+  document.title = to.matched[0].meta.title //给目标路由的页面的title赋值
+  next()//必须调用，不调用不会跳转
+})
+
+/**
+ * 后置钩子
+ */
+ router.afterEach((to, from) => {
+  console.log('后置钩子调用了----')
+})
+
 
 export default router
