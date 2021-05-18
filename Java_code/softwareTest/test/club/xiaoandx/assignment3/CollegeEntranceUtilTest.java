04cc518b9d8b4b7e3df70b4a13f2f8cf75fe8758
WEI.ZHOU <2324671838@qq.com>
2021-05-18 14:18:07 +0800

    feat softwareTest : 添加高考报名黑盒测试案例

--- Java_code/softwareTest/test/club/xiaoandx/assignment3/CollegeEntranceUtilTest.java
+++ Java_code/softwareTest/test/club/xiaoandx/assignment3/CollegeEntranceUtilTest.java
@@ -65,5 +65,186 @@ public class CollegeEntranceUtilTest {
 		boolean rust = false;
 		assertEquals(registrationResults, rust);
 	}
+	
+	@Test
+	public void notEquivalenceTest05() {
+		boolean registrationResults = CollegeEntranceUtil.registrationJudgment("1996-00");
+		boolean rust = false;
+		assertEquals(registrationResults, rust);
+	}
+	
+	@Test
+	public void notEquivalenceTest06() {
+		boolean registrationResults = CollegeEntranceUtil.registrationJudgment("2005-13");
+		boolean rust = false;
+		assertEquals(registrationResults, rust);
+	}
+	
+	@Test
+	public void notEquivalenceTest07() {
+		boolean registrationResults = CollegeEntranceUtil.registrationJudgment("200a-0a");
+		boolean rust = false;
+		assertEquals(registrationResults, rust);
+	}
+	
+	@Test
+	public void notEquivalenceTest08() {
+		boolean registrationResults = CollegeEntranceUtil.registrationJudgment("-2001-2");
+		boolean rust = false;
+		assertEquals(registrationResults, rust);
+	}
+	
+	@Test
+	public void notEquivalenceTest09() {
+		boolean registrationResults = CollegeEntranceUtil.registrationJudgment("199812");
+		boolean rust = false;
+		assertEquals(registrationResults, rust);
+	}
+	
+	@Test
+	public void notEquivalenceTest10() {
+		boolean registrationResults = CollegeEntranceUtil.registrationJudgment("200-12");
+		boolean rust = false;
+		assertEquals(registrationResults, rust);
+	}
+	
+	@Test
+	public void notEquivalenceTest11() {
+		boolean registrationResults = CollegeEntranceUtil.registrationJudgment("2000-2");
+		boolean rust = false;
+		assertEquals(registrationResults, rust);
+	}
+	
+	@Test
+	public void boundaryValueTest01() {
+		boolean registrationResults = CollegeEntranceUtil.registrationJudgment("5");
+		boolean rust = false;
+		assertEquals(registrationResults, rust);
+	}
+	
+	@Test
+	public void boundaryValueTest02() {
+		boolean registrationResults = CollegeEntranceUtil.registrationJudgment("96");
+		boolean rust = false;
+		assertEquals(registrationResults, rust);
+	}
+	
+	@Test
+	public void boundaryValueTest03() {
+		boolean registrationResults = CollegeEntranceUtil.registrationJudgment("200");
+		boolean rust = false;
+		assertEquals(registrationResults, rust);
+	}
+	
+	@Test
+	public void boundaryValueTest04() {
+		boolean registrationResults = CollegeEntranceUtil.registrationJudgment("2000");
+		boolean rust = false;
+		assertEquals(registrationResults, rust);
+	}
+	
+	@Test
+	public void boundaryValueTest05() {
+		boolean registrationResults = CollegeEntranceUtil.registrationJudgment("2000-1");
+		boolean rust = false;
+		assertEquals(registrationResults, rust);
+	}
+	
+	@Test
+	public void boundaryValueTest06() {
+		boolean registrationResults = CollegeEntranceUtil.registrationJudgment("2000-12");
+		boolean rust = true;
+		assertEquals(registrationResults, rust);
+	}
+	
+	@Test
+	public void boundaryValueTest07() {
+		boolean registrationResults = CollegeEntranceUtil.registrationJudgment("2010");
+		boolean rust = false;
+		assertEquals(registrationResults, rust);
+	}
+	
+	@Test
+	public void boundaryValueTest08() {
+		boolean registrationResults = CollegeEntranceUtil.registrationJudgment("1992");
+		boolean rust = false;
+		assertEquals(registrationResults, rust);
+	}
 
+	@Test
+	public void boundaryValueTest09() {
+		boolean registrationResults = CollegeEntranceUtil.registrationJudgment("-1");
+		boolean rust = false;
+		assertEquals(registrationResults, rust);
+	}
+	
+	@Test
+	public void boundaryValueTest10() {
+		boolean registrationResults = CollegeEntranceUtil.registrationJudgment("2000-10");
+		boolean rust = true;
+		assertEquals(registrationResults, rust);
+	}
+	
+	@Test
+	public void boundaryValueTest11() {
+		boolean registrationResults = CollegeEntranceUtil.registrationJudgment("2000-13");
+		boolean rust = false;
+		assertEquals(registrationResults, rust);
+	}
+	
+	@Test
+	public void boundaryValueTest12() {
+		boolean registrationResults = CollegeEntranceUtil.registrationJudgment("2000-00");
+		boolean rust = false;
+		assertEquals(registrationResults, rust);
+	}
+	
+	@Test
+	public void boundaryValueTest13() {
+		boolean registrationResults = CollegeEntranceUtil.registrationJudgment("1996-05");
+		boolean rust = false;
+		assertEquals(registrationResults, rust);
+	}
+	
+	@Test
+	public void boundaryValueTest14() {
+		boolean registrationResults = CollegeEntranceUtil.registrationJudgment("2005-09");
+		boolean rust = false;
+		assertEquals(registrationResults, rust);
+	}
+	
+	@Test
+	public void errorValueTest01() {
+		boolean registrationResults = CollegeEntranceUtil.registrationJudgment("0000-00");
+		boolean rust = false;
+		assertEquals(registrationResults, rust);
+	}
+	
+	@Test
+	public void errorValueTest02() {
+		boolean registrationResults = CollegeEntranceUtil.registrationJudgment("");
+		boolean rust = false;
+		assertEquals(registrationResults, rust);
+	}
+	
+	@Test
+	public void errorValueTest03() {
+		boolean registrationResults = CollegeEntranceUtil.registrationJudgment("12-2000");
+		boolean rust = false;
+		assertEquals(registrationResults, rust);
+	}
+	
+	@Test
+	public void errorValueTest04() {
+		boolean registrationResults = CollegeEntranceUtil.registrationJudgment("200-012");
+		boolean rust = false;
+		assertEquals(registrationResults, rust);
+	}
+	
+	@Test
+	public void errorValueTest05() {
+		boolean registrationResults = CollegeEntranceUtil.registrationJudgment("-200012");
+		boolean rust = false;
+		assertEquals(registrationResults, rust);
+	}
 }
